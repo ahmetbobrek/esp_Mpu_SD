a4ace841ed2cb6c9d3fae8141785541880175d83
DESKTOP-STBF4HO\ahmet <ahmetbobrek@gmail.com>
2022-07-04 14:39:13 +0300

    040702

--- esp_Mpu_SD/src/main.cpp
+++ esp_Mpu_SD/src/main.cpp
@@ -1,4 +1,11 @@
 #include <Arduino.h>
+#include "Wire.h"
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+
 
 void setup() {
   // put your setup code here, to run once:
